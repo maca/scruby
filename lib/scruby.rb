1df45947cd7af52e38f5fb3e4f2dbe21b77c2567
Macario <macarui@gmail.com>
2009-08-31 04:20:33 -0500

    version bump

--- lib/scruby.rb
+++ lib/scruby.rb
@@ -25,7 +25,7 @@ require 'yaml'
 $:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Scruby
-  VERSION = '0.2.6.7'
+  VERSION = '0.2.6.8'
 end
 
 require "scruby/core_ext/object"
--- scruby.gemspec
+++ scruby.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |s|
   s.name = %q{scruby}
-  s.version = "0.2.6.7"
+  s.version = "0.2.6.8"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Macario Ortega"]
