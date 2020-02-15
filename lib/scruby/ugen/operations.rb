443cf23ed8ed52ace006b4d7dc98b329dc1054e7
Macario <maca@aelita.io>
2020-02-15 23:08:53 +0100

    operation aliases

--- lib/scruby/ugen/operations.rb
+++ lib/scruby/ugen/operations.rb
@@ -16,7 +16,7 @@ module Scruby
           unary_indexes.keys + binary_indexes.keys
         end
 
-        def unary(name, index: )
+        def unary(name, index:, aliases: [])
           unary_indexes[name] = index
 
           define_method(name) do
@@ -26,9 +26,17 @@ module Scruby
           OperationHelpers.define_method name do |operand|
             Operations.apply_unary(name, operand)
           end
+
+          aliases.each do |name_alias|
+            define_method(name_alias) { send(name) }
+
+            OperationHelpers.define_method name_alias do |operand|
+              send(name, operand)
+            end
+          end
         end
 
-        def binary(name, index: , operator: nil)
+        def binary(name, index:, operator: nil, aliases: [])
           binary_indexes[name] = index
 
           define_method name do |input|
@@ -40,6 +48,16 @@ module Scruby
           end
 
           define_method(operator) { |i| send(name, i) } if operator
+
+          aliases.each do |name_alias|
+            define_method name_alias do |input|
+              send(name, input)
+            end
+
+            OperationHelpers.define_method name_alias do |left, right|
+              send(name, left, right)
+            end
+          end
         end
 
         def apply_unary(name, operand)
@@ -62,10 +80,15 @@ module Scruby
         private
 
         def max_rate(left, right)
-          index = [ left.rate, right.rate ]
+          index = [ rate(left), rate(right) ]
                     .map { |rate| Ugen::RATES.index(rate) }.max
 
           Ugen::RATES.fetch(index)
+        end
+
+        def rate(operand)
+          return operand.rate if operand.respond_to?(:rate)
+          :scalar
         end
       end
 
@@ -80,19 +103,19 @@ module Scruby
       unary :cubed, index: 13
       unary :sqrt, index: 14
       unary :exp, index: 15
-      unary :midicps, index: 17
-      unary :cpsmidi, index: 18
-      unary :midiratio, index: 19
-      unary :ratiomidi, index: 20
-      unary :dbamp, index: 21
-      unary :ampdb, index: 22
-      unary :octcps, index: 23
-      unary :cpsoct, index: 24
+      unary :midicps, index: 17, aliases: [ :midi_cps ]
+      unary :cpsmidi, index: 18, aliases: [ :cps_midi ]
+      unary :midiratio, index: 19, aliases: [ :midi_ration ]
+      unary :ratiomidi, index: 20, aliases: [ :ratio_midi ]
+      unary :dbamp, index: 21, aliases: [ :db_amp ]
+      unary :ampdb, index: 22, aliases: [ :amp_dpb ]
+      unary :octcps, index: 23, aliases: [ :oct_cps ]
+      unary :cpsoct, index: 24, aliases: [ :cps_oct ]
       unary :rand, index: 37
       unary :rand2, index: 38
-      unary :linrand, index: 39
-      unary :bilinrand, index: 40
-      unary :sum3rand, index: 41
+      unary :linrand, index: 39, aliases: [ :lind_rand ]
+      unary :bilinrand, index: 40, aliases: [ :biling_rand ]
+      unary :sum3rand, index: 41, aliases: [ :sum_3_rand ]
       unary :coin, index: 44
       unary :log, index: 25
       unary :log2, index: 26
@@ -107,7 +130,7 @@ module Scruby
       unary :cosh, index: 35
       unary :tanh, index: 36
       unary :distort, index: 42
-      unary :softclip, index: 43
+      unary :softclip, index: 43, aliases: [ :soft_clip ]
 
       binary :add, index: 0, operator: :+
       binary :sub, index: 1, operator: :-
@@ -127,27 +150,27 @@ module Scruby
       binary :round, index: 19
       binary :trunc, index: 21
       binary :hypot, index: 23
-      binary :hypotApx, index: 24
+      binary :hypotApx, index: 24, aliases: [ :hypot_apx, :hypotapx ]
       binary :atan2, index: 22
       binary :ring1, index: 30
       binary :ring2, index: 31
       binary :ring3, index: 32
       binary :ring4, index: 33
-      binary :sumsqr, index: 35
-      binary :difsqr, index: 34
-      binary :sqrsum, index: 36
-      binary :sqrdif, index: 37
-      binary :absdif, index: 38
-      binary :moddif, index: -1
+      binary :sumsqr, index: 35, aliases: [ :sum_sqr ]
+      binary :difsqr, index: 34, aliases: [ :diff_sqr ]
+      binary :sqrsum, index: 36, aliases: [ :sqr_sum ]
+      binary :sqrdif, index: 37, aliases: [ :sqr_dif ]
+      binary :absdif, index: 38, aliases: [ :abs_dif ]
       binary :thresh, index: 39
       binary :amclip, index: 40
-      binary :scaleneg, index: 41
+      binary :scaleneg, index: 41, aliases: [ :scale_neg ]
       binary :clip2, index: 42
       binary :wrap2, index: 45
       binary :fold2, index: 44
       binary :excess, index: 43
 
 
+      # binary :moddif, index: -1, aliases: [:mod_dif]
       # unary :is_positive, index: -1
       # unary :is_negative, index: -1
       # unary :is_strictly_positive, index: -1
