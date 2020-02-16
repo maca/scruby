fbf559fd1c6e623ad6c10f0ccdba7ae998a2deb8
Macario <maca@aelita.io>
2020-02-16 23:52:43 +0100

    multichannel specs

--- spec/ugen/integration/multichannel_graph_spec.rb
+++ spec/ugen/integration/multichannel_graph_spec.rb
@@ -57,4 +57,36 @@ RSpec.describe Graph do
       expect(graph.encode).to eq(expected)
     end
   end
+
+  describe "simple nested multichannel" do
+    let(:graph) do
+      Graph.new(
+        Out.ar(0, SinOsc.ar(SinOsc.ar([242, 442]))),
+        name: "multichannel"
+      )
+    end
+
+    it { expect(graph.nodes.map(&:name))
+           .to eq %w(SinOsc SinOsc SinOsc SinOsc Out) }
+
+    it { expect(graph.constants.map(&:value)).to eq [ 242, 0, 442 ] }
+
+    let(:expected) do
+      [
+        83, 67, 103, 102, 0, 0, 0, 2, 0, 1, 12, 109, 117, 108, 116,
+        105, 99, 104, 97, 110, 110, 101, 108, 0, 0, 0, 3, 67, 114, 0,
+        0, 0, 0, 0, 0, 67, -35, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        3, 6, 83, 105, 110, 79, 115, 99, 2, 0, 0, 0, 2, 0, 0, 0, 1, 0,
+        0, -1, -1, -1, -1, 0, 0, 0, 0, -1, -1, -1, -1, 0, 0, 0, 1, 2,
+        6, 83, 105, 110, 79, 115, 99, 2, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0,
+        -1, -1, -1, -1, 0, 0, 0, 2, -1, -1, -1, -1, 0, 0, 0, 1, 2, 3,
+        79, 117, 116, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1,
+        0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0
+      ].pack("C*")
+    end
+
+    # it "should encode graph" do
+    #   expect(graph.encode).to eq(expected)
+    # end
+  end
 end
