9d81941372538b9542dc17eab68bbb41d3ff35fc
Macario <maca@aelita.io>
2020-02-16 22:23:14 +0100

    graph node refactor

--- lib/scruby/graph/node.rb
+++ lib/scruby/graph/node.rb
@@ -19,7 +19,7 @@ module Scruby
       def initialize(ugen, inputs, graph)
         @ugen = ugen
         @graph = graph
-        @inputs = inputs.flatten
+        @inputs = map_inputs(inputs, graph).flatten
 
         graph.add_node(self)
       end
@@ -53,7 +53,7 @@ module Scruby
       end
 
       def inspect
-        super(ugen: ugen)
+        super(name: name, inputs: inputs)
       end
 
       private
@@ -61,12 +61,41 @@ module Scruby
       def output_index; 0 end
       def special_index; 0 end
 
+      def map_inputs(inputs, graph)
+        inputs.map { |e| map_node(e, graph)}
+          .map { |e| map_constant(e, graph) }
+          .map { |e| map_control_name(e, graph) }
+          .map { |e| map_control(e, graph) }
+      end
+
+      def map_node(elem, graph)
+        return elem unless elem.is_a?(Ugen::Base)
+        Node.build(elem, graph)
+      end
+
+      def map_constant(elem, graph)
+        return elem unless elem.is_a?(Numeric)
+        Constant.new(elem).tap { |const| graph.add_constant(const) }
+      end
+
+      def map_control_name(name, graph)
+        return name unless [ String, Symbol ]
+                              .any? { |t| name.is_a?(t) }
+
+        graph.control_name(name)
+      end
+
+      def map_control(elem, graph)
+        return elem unless elem.is_a?(ControlName)
+        graph.add_control(elem) && elem
+      end
+
       class << self
         def build(ugen, graph)
           values = ugen.input_values
 
           unless values.any? { |e| e.is_a?(Array) }
-            return new(ugen, map_inputs(values, graph), graph)
+            return new(ugen, values, graph)
           end
 
           wrapped = values.map { |elem| [*elem] }
@@ -74,38 +103,7 @@ module Scruby
 
           wrapped.map { |elem| elem.cycle.take(size) }
             .transpose
-            .map { |ins| new(ugen, map_inputs(ins, graph), graph) }
-        end
-
-        private
-
-        def map_inputs(inputs, graph)
-          inputs.map { |e| map_node(e, graph)}
-            .map { |e| map_constant(e, graph) }
-            .map { |e| map_control_name(e, graph) }
-            .map { |e| map_control(e, graph) }
-        end
-
-        def map_node(elem, graph)
-          return elem unless elem.is_a?(Ugen::Base)
-          Node.build(elem, graph)
-        end
-
-        def map_constant(elem, graph)
-          return elem unless elem.is_a?(Numeric)
-          Constant.new(elem).tap { |const| graph.add_constant(const) }
-        end
-
-        def map_control_name(name, graph)
-          return name unless [ String, Symbol ]
-                               .any? { |t| name.is_a?(t) }
-
-          graph.control_name(name)
-        end
-
-        def map_control(elem, graph)
-          return elem unless elem.is_a?(ControlName)
-          graph.add_control(elem) && elem
+            .map { |inputs| new(ugen, inputs, graph) }
         end
       end
     end
