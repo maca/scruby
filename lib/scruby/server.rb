18b3d42aec6f957b6f46e130a9deb984e9566ebc
Macario <maca@aelita.io>
2020-03-27 16:44:51 +0100

    server process

--- lib/scruby/server.rb
+++ lib/scruby/server.rb
@@ -9,7 +9,7 @@ module Scruby
     include PrettyInspectable
     include Encode
 
-    attr_reader :host, :port, :client, :message_queue, :process, :name
+    attr_reader :host, :port, :client, :message_queue, :process
     private :process
 
 
@@ -17,44 +17,52 @@ module Scruby
       @host = host
       @port = port
       @message_queue = MessageQueue.new(self)
-      @client = Client.new(port, host)
-      @name = "~scruby_server_#{object_id}"
+      @client = OSC::Client.new(port, host)
     end
 
-    def boot(**opts)
-      Sclang.main.spawn
-        .then { eval_boot(opts) }.flat_future
-        .then { message_queue.sync }.flat_future
+    def boot(binary: "scsynth", **opts)
+      flags = Options.new(**opts, **{ bind_address: host }).flags
+      @process = Process.spawn(binary, flags)
+
+      message_queue.sync
+        .then { continue_boot }
         .then { self }
     end
 
-    def boot!(**opts)
-      boot(**opts).value!
+
+    def boot!(binary: "scsynth", **opts)
+      boot(binary: binary, **opts).value!
     end
 
-    def quit
-      forward_async :quit
+
+    def continue_boot
+      send_msg "/g_new", 1
     end
 
-    def reboot
-      forward_async :reboot
-    end
+    # def quit
+    #   forward_async :quit
+    # end
 
-    def free_all
-      forward_async :freeAll
-    end
+    # def reboot
+    #   forward_async :reboot
+    # end
 
-    def mute
-      forward_async :mute
-    end
+    # def free_all
+    #   forward_async :freeAll
+    # end
 
-    def unmute
-      forward_async :unmute
-    end
+    # def mute
+    #   forward_async :mute
+    # end
+
+    # def unmute
+    #   forward_async :unmute
+    # end
 
     def dump_osc(code = 1)
       send_msg("/dumpOSC", code)
     end
+
 
     # Sends an OSC command or +Message+ to the scsyth server.
     # E.g. +server.send('/dumpOSC', 1)+
@@ -91,37 +99,6 @@ module Scruby
     def graph_completion_blob(message)
       return message || 0 unless message.is_a? Message
       Blob.new(message.encode)
-    end
-
-    def eval_async(code)
-      Sclang.main.eval_async(code)
-    end
-
-    def forward_async(method)
-      eval_async "#{name}.#{Sclang::Helpers.camelize(method.to_s)}"
-    end
-
-    def eval_boot(opts)
-      options = Options.new(**opts, **{ bind_address: host })
-
-      opts_assigns = options.map do |k, v|
-        "opts.#{camelize(k)} = #{literal(v)}"
-      end
-
-
-      sclang_boot = <<-SC
-        { var addr = NetAddr.new("#{options.address}", #{port}),
-              opts = ServerOptions.new;
-          #{opts_assigns.join(";\n")};
-          #{name} = Server.new("#{name}", addr, opts);
-          #{name}.boot;
-        }.value
-      SC
-
-      eval_async(sclang_boot).then do |line|
-        next line unless /error/i === line
-        raise SclangError, "server could not be booted"
-      end
     end
 
     class << self
